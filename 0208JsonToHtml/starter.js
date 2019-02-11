969b4e4d0a5be5956abf3dc34ec88bc4e93725fc
RaimondasA <ra.alisa.123@gmail.com>
2019-02-11 10:40:58 +0200

    Code cleaning

--- 0208JsonToHtml/starter.js
+++ 0208JsonToHtml/starter.js
@@ -1,18 +1,19 @@
+"use strict"
 // let test = makeNodeFromJson(testJson);
 // insertNodeInHtml("start", test)
+
+window.onload = () => {
+    insertNodeInHtml("start", bodyNode)
+    insertNodeInHtml("navbar", navbarNode);
+    insertNodeInHtml("article", articleNode1);
+    appendChildInHtml("article", modalNode);
+};
 
 let bodyNode = makeNodeFromJson(bodyJson);
 let navbarNode = makeNodeFromJson(navbarJson);
 let articleNode1 = makeNodeFromJson(articleJson1);
-let articleNode2 = makeNodeFromJson(articleJson2);
 let modalNode = makeNodeFromJson(modalJson);
-
-insertNodeInHtml("start", bodyNode)
-insertNodeInHtml("navbar", navbarNode);
-insertNodeInHtml("article", articleNode1);
-appendChildInHtml("article", modalNode);
-
-
+let articleNode2 = makeNodeFromJson(articleJson2);
 
 //Router
 window.onhashchange = () => {
@@ -77,22 +78,81 @@ function jsonToHtml(json, parent) {
 
             case (value.constructor === Array):
                 value.map(element => {
-                    let childA = document.createElement(key);
-                    jsonToHtml(element, childA);
-                    parent.appendChild(childA);
+                    let child = document.createElement(key);
+                    jsonToHtml(element, child);
+                    parent.appendChild(child);
                     // console.log("PARENT ARRAY= ", parent);
                 });
                 break;
 
             default:
-                let childB = document.createElement(key);
+                let child = document.createElement(key);
                 if (typeof value == "object") {
-                    jsonToHtml(value, childB);
+                    jsonToHtml(value, child);
                 }
-                parent.appendChild(childB);
+                parent.appendChild(child);
                 // console.log("PARENT DEFAULT= ", parent);
         }
     }
     // console.log("PARENT RETURN= ", parent)
     return parent;
 }
+
+/*
+async function makeNodeFromJson(path) {
+    let jsonfile = await fetchJson(path);
+    let jsonPars = JSON.parse(jsonfile);
+    let startNode = document.createElement('div')
+    let node = nodeToString(jsonToHtml(jsonPars, startNode));
+    return node;
+}
+
+
+async function fetchJson(path) {
+    let jsonResponse = {};
+    await fetch(path)
+        .then(response => response.json())
+        .then(data => jsonResponse = data);
+    console.log('jsonResponse', jsonResponse);
+    return jsonResponse;
+}
+
+function readJSON(path) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function (e) {
+        if (this.status == 200) {
+            let file = new File([this.response], 'temp');
+            let fileReader = new FileReader();
+            fileReader.addEventListener('load', function () {
+                //do stuff with fileReader.result
+                return fileReader.result
+            });
+            fileReader.readAsText(file);
+        }
+    }
+    xhr.send();
+}
+
+
+function loadJSON(path, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function init() {
+    loadJSON(function (response) {
+        console.log('b', response);
+        var actual_JSON = JSON.parse(response);
+    });
+}
+
+*/
